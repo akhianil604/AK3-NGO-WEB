13634f50dcd90e3536f5a31812d4ba2225881303
Akshaj L Shastry <151350419+als1810@users.noreply.github.com>
2024-11-07 10:02:00 +0530

    Add files via upload

---
+++ ak3-ngo/src/components/User-Login/user-login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './project.css';
+
+function UserPortal() {
+    return (
+        <div className="user-background">
+            <section id="user-portal-header">
+                <div className="header">
+                    <h1>Welcome to the User Login</h1>
+                    <p>This is the user portal where individuals can track their registered pleas and access other services offered by our NGO.</p>
+                    <a href="/" className="back-btn">Go Back to (AK)<sup>3</sup>'s NGO Home Page</a>
+                </div>
+            </section>
+
+            <section id="user-portal-login">
+                <p className="uplf_h">Enter your Credentials</p>
+                <form className="user-portal-login-form">
+                    <div className="uplf1">
+                        <label htmlFor="ID">ID:</label>
+                        <input type="text" id="ID" name="ID" placeholder="ID given by the NGO" required />
+                    </div>
+
+                    <div className="uplf2">
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" name="password" placeholder="Password used during sign-up" required />
+                    </div>
+
+                    <button type="submit" className="login-btn-u">Sign In</button>
+                </form>
+            </section>
+        </div>
+    );
+}
+
+export default UserPortal;
